ef1ec0b6a1a19accc2053b9a464c7b437af7412f
Tyler Knapp <knapptr@gmail.com>
2023-06-19 07:36:11 -0400

    Removes problematic fetch requests

--- src/pages/SignUpIndex.js
+++ src/pages/SignUpIndex.js
@@ -4,47 +4,10 @@ import useGetDataOnMount from "../hooks/useGetData";
 import "styled-components/macro";
 // import cl from "../cl.png";
 
+/** This once was something great...but now, it's just an outlet */
 const SignUpIndex = () => {
-	// const location = useLocation();
-	const navigate = useNavigate();
-	const [cabins] = useGetDataOnMount({
-		url: "/api/cabins",
-		initialState: [],
-		optionalSortFunction: (c1, c2) => {
-			if ((c1.area = "GA")) {
-				return 1;
-			} else {
-				return -1;
-			}
-		},
-		useToken: true,
-	});
-	const [weeks] = useGetDataOnMount({
-		url: "/api/weeks",
-		initialState: [],
-		useToken: true,
-	});
-	const [selected] = useState({
-		cabin: undefined,
-		week: undefined,
-	});
-	// const select = (name, value) => {
-	// 	setSelected((s) => ({ ...s, [name]: value }));
-	// };
-	useEffect(() => {
-		if (selected.cabin && selected.week) {
-
-			navigate(`/schedule/sign-up/${selected.cabin}/${selected.week}`)
-		}
-	}, [navigate,selected.cabin, selected.week])
 	return (
 		<>
-			{!(weeks.length > 0 && cabins.length > 0) ? (
-				<>
-					<p>loading...</p>
-				</>
-			) : (
-				<></>)}
 			<Outlet />
 		</>
 	);

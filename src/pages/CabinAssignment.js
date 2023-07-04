b43e036be443bca7bf603cf78f85686f3cbe0abf
Tyler Knapp <knapptr@gmail.com>
2023-07-04 07:59:25 -0400

    Add 7th week to assigment pages

--- src/pages/CabinAssignment.js
+++ src/pages/CabinAssignment.js
@@ -27,7 +27,7 @@ import usePops from "../hooks/usePops";
 
 // CONSTANTS
 const areas = ["ba", "ga"];
-const weeks = ["1", "2", "3", "4", "5", "6"];
+const weeks = ["1", "2", "3", "4", "5", "6", "7"];
 const drawerWidth = 1 / 3;
 const marginSize = 56;
 

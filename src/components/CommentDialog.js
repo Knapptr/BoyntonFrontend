02bc6d1c9ef480f6818b47c54d618479bfec4f94
Tyler Knapp <knapptr@gmail.com>
2025-08-03 19:58:38 -0400

    Adds dated comments

--- src/components/CommentDialog.js
+++ src/components/CommentDialog.js
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogTitle, Stack, TextField } from "@mui/material";
+import { Button, Checkbox, Dialog, DialogContent, DialogTitle, FormControlLabel, FormGroup, Stack, TextField } from "@mui/material";
 import { useContext, useState } from "react";
 import fetchWithToken from "../fetchWithToken";
 import usePops from "../hooks/usePops";
@@ -8,9 +8,11 @@ const CommentDialog = ({ open, camper, onPush, toggleCommentModal }) => {
   const { shamefulFailure, PopsBar } = usePops();
   const auth = useContext(UserContext);
   const [content, setContent] = useState("");
+  const [dueDate, setDueDate] = useState(null);
+  const [hasDate, setHasDate] = useState(false);
   const [error, setError] = useState(false);
   const sendCommentToServer = async () => {
-    const data = { content, camperId: camper.id };
+    const data = { content, camperId: camper.id, dueDate   };
     const url = "/api/camper-comment";
     const config = {
       method: "POST",
@@ -27,10 +29,21 @@ const CommentDialog = ({ open, camper, onPush, toggleCommentModal }) => {
     }
   };
 
+  const toggleHasDate = () =>{
+    setHasDate((d)=>{return !d})
+  }
+
   const clearContent = () => {
     setContent("");
+    setHasDate(null);
     setError(false);
   };
+  const handleDueDateChange = (e) =>{
+    setDueDate(()=>{
+      const newDate = e.target.value;
+    return newDate;
+    })
+  }
   const handleContentChange = (e) => {
     setContent(() => {
       const newData = e.target.value;
@@ -58,6 +71,12 @@ const CommentDialog = ({ open, camper, onPush, toggleCommentModal }) => {
           value={content}
           onChange={handleContentChange}
         />
+
+    <FormGroup> 
+    <FormControlLabel control={<Checkbox checked={hasDate} onChange={toggleHasDate}  />} label="Has Due Date" />
+    </FormGroup>
+    
+    <input type="date" onChange={handleDueDateChange}disabled={!hasDate} hidden={!hasDate} /> 
       </DialogContent>
       <Stack direction="row">
         <Button
